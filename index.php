e9b20bba99ab2aa6902b644c5bb825cab93343f3
MikiWojak <mikizarowa8@gmail.com>
2024-04-07 09:22:43 +0200

    Extend routing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // @TODO Refactor router
-require 'src/controllers/AppController.php';
+require_once 'src/controllers/AppController.php';
 
 $controller = new AppController();
 
@@ -9,5 +9,28 @@ $controller = new AppController();
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 $action = explode("/", $path)[0];
+$action = $action == null ? 'login' : $action;
 
-$controller->render($action);
+// @TODO Refactor
+// Objects?
+switch($action) {
+    case "dashboard":
+        $films = [
+            'The Wind Rises',
+            '500',
+            'Psy'
+        ];
+
+        $controller->render($action, [
+                "items" => $films,
+                "title" => "Films"
+        ]);
+
+        break;
+    case "login":
+        $controller->render($action);
+
+        break;
+    default:
+        $controller->render($action);
+}
--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -29,7 +29,7 @@ class AppController {
         if(file_exists($templatePath)){
             // Extract variables
             extract($variables);
-            
+
             ob_start();
             include $templatePath;
             $output = ob_get_clean();
