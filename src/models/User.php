9cc8af4b4fea543f7ad9d2ea42a759476b8ed979
MikiWojak <mikizarowa8@gmail.com>
2024-04-17 18:31:09 +0200

    Class User

---
+++ src/models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+class User
+{
+    private $username;
+    private $email;
+    private $password;
+
+    public function __construct(
+        string $username,
+        string $email,
+        string $password)
+    {
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+
+}
