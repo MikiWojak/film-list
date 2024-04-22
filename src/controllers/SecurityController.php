920182a65491eb52e1751abb93295ecbe4a4ae2e
MikiWojak <mikizarowa8@gmail.com>
2024-04-22 20:04:13 +0200

    Find User by email

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,16 +2,13 @@
 
 require_once 'AppController.php';
 require_once  __DIR__.'/../models/User.php';
+require_once  __DIR__.'/../repositories/UserRepository.php';
 
 class SecurityController extends AppController
 {
     public function login()
     {
-        $user = new User(
-            'johndoe',
-            'john.doe@film.test',
-            'test'
-        );
+        $userRepository = new UserRepository();
 
         if(!$this->isPost()) {
             return $this->render('login');
@@ -19,6 +16,12 @@ class SecurityController extends AppController
 
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $user =  $userRepository->findByEmail($email);
+
+        if(!$user) {
+            return $this->render('login', ['messages' => ['User not exists']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email does not exists']]);
