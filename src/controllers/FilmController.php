8f40990590fb12bbc401f05ce373eb01faa4e893
MikiWojak <mikizarowa8@gmail.com>
2024-04-25 17:28:47 +0200

    Refactor controller

--- src/controllers/FilmController.php
+++ src/controllers/FilmController.php
@@ -27,15 +27,20 @@ class FilmController extends AppController
     public function search() {
         $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
-        // @TODO Refactor
-        if($contentType === "application/json") {
-            $content = trim(file_get_contents("php://input"));
-            $decoded = json_decode($content, true);
-
-            header('Content-type: application/json');
-            http_response_code(200);
-
-            echo json_encode($this->filmRepository->findAllByTitle($decoded["search"]));
+        if($contentType !== "application/json") {
+            // @TODO Show error message
+            $this->render('films', [
+                "films" => $this->filmRepository->findAll(),
+                "title" => "Films"
+            ]);
         }
+
+        $content = trim(file_get_contents("php://input"));
+        $decoded = json_decode($content, true);
+
+        header('Content-type: application/json');
+        http_response_code(200);
+
+        echo json_encode($this->filmRepository->findAllByTitle($decoded["search"]));
     }
 }
