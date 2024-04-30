312eb6bc3ab1ae8720753d13d8876a276be0e9ac
MikiWojak <mikizarowa8@gmail.com>
2024-04-30 20:09:46 +0200

    Refactor script

--- public/js/rateButtonsSetup.js
+++ public/js/rateButtonsSetup.js
@@ -2,16 +2,15 @@ export const setupRateButtons = () => {
     const rateModal = document.querySelector("#rate-modal");
     const rateButtons = document.querySelectorAll("#rate__btn");
 
-    rateButtons.forEach(rateButton => {
-        // @TODO Arrow function
-        rateButton.addEventListener("click", function () {
-            const id = this.getAttribute("data-id");
-            const title = this.getAttribute("data-title");
+    rateButtons.forEach(rateButton =>
+        rateButton.addEventListener("click", () => {
+            const id = rateButton.getAttribute("data-id");
+            const title = rateButton.getAttribute("data-title");
 
             rateModal.querySelector("#modal-film-id").innerText = id;
             rateModal.querySelector("#modal-film-title").innerText = title;
 
             rateModal.classList.add('enabled');
-        });
-    });
+        })
+    );
 }
