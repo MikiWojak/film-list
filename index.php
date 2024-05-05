cbeee0d905e6884c1b70f417bf6403c71a286f76
MikiWojak <mikizarowa8@gmail.com>
2024-05-05 13:31:03 +0200

    Changes

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once 'src/controllers/AdminDirectorController.php';
 
 $controller = new AppController();
 
+// Process URL from client --> Process request from client
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
@@ -31,7 +32,7 @@ Routing::post('search', 'FilmController');
 
 if (isLoggedIn()) {
     Routing::post('logout', 'SecurityController');
-    Routing::post('profile', 'SecurityController');
+    Routing::get('profile', 'SecurityController');
 
     // @TODO Rate - User
 
