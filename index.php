61022eee587c3b24be1d7c73417b2bfedf627fe9
MikiWojak <mikizarowa8@gmail.com>
2024-05-05 12:34:41 +0200

    Role based authorization

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'Routing.php';
+require_once 'src/models/Role.php';
 require_once 'src/controllers/FilmController.php';
 require_once 'src/controllers/SecurityController.php';
 require_once 'src/controllers/AdminFilmController.php';
@@ -9,24 +10,40 @@ require_once 'src/controllers/AdminDirectorController.php';
 
 $controller = new AppController();
 
-// Process URL from client --> Process request from client
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
+
+function isLoggedIn() {
+    return isset($_SESSION['loggedUser']);
+}
+
+function hasRole($role) {
+    if (!isLoggedIn()) return false;
+
+    $loggedUser = unserialize($_SESSION['loggedUser']);
+
+    return $loggedUser->hasRole($role);
+}
 
 Routing::get('', 'FilmController');
 Routing::get('film', 'FilmController');
 Routing::post('search', 'FilmController');
 
-Routing::post('login', 'SecurityController');
-Routing::post('logout', 'SecurityController');
-Routing::post('profile', 'SecurityController');
-Routing::post('register', 'SecurityController');
+if (isLoggedIn()) {
+    Routing::post('logout', 'SecurityController');
+    Routing::post('profile', 'SecurityController');
 
-Routing::get('adminfilms', 'AdminFilmController');
-Routing::post('admincreatefilm', 'AdminFilmController');
+    // @TODO Rate - User
 
-Routing::get('adminusers', 'AdminUserController');
-
-Routing::get('admindirectors', 'AdminDirectorController');
+    if (hasRole(ROLE::ROLE_ADMIN)) {
+        Routing::get('adminfilms', 'AdminFilmController');
+        Routing::post('admincreatefilm', 'AdminFilmController');
+        Routing::get('adminusers', 'AdminUserController');
+        Routing::get('admindirectors', 'AdminDirectorController');
+    }
+} else {
+    Routing::post('login', 'SecurityController');
+    Routing::post('register', 'SecurityController');
+}
 
 Routing::run($path);
