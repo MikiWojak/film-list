8ae6529de36a585177cab2b41aa5f30a73d09541
MikiWojak <mikizarowa8@gmail.com>
2024-05-18 11:42:34 +0200

    Not found - changes

--- Routing.php
+++ Routing.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__ .'/src/controllers/DefaultController.php';
-
 class Routing {
     public static $routes;
 
@@ -18,22 +16,24 @@ class Routing {
         $action = $urlParts[0];
 
         if (!array_key_exists($action, self::$routes)) {
-            $object = new DefaultController();
+            $errController = 'DefaultController';
+            $errObject = new $errController;
+            $errAction = 'notFound';
 
-            return $object->notFound();
+            return $errObject->$errAction();
         }
 
         $controller = self::$routes[$action];
         // Object based on String
-        $object = new $controller;
+        $errObject = new $controller;
         $action = $action ?: 'index';
 
         $id = $urlParts[1] ?? '';
 
         if (preg_match("/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i", $id)) {
-            $object->$action($id);
+            $errObject->$action($id);
         } else {
-            $object->$action();
+            $errObject->$action();
         }
     }
 }
